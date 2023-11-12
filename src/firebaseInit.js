13b1cfeac2052a2b0a77d49a974f913c1937328a
alexander <lokistapway@yandex.ru>
2023-11-12 21:07:59 +0300

    Сделано подключение firebase

---
+++ src/firebaseInit.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseInit = {
+  apiKey: "AIzaSyC6Q3S3u3Ar97fQPrb1FM82C7dIf8BvXv4",
+  authDomain: "memorial-book-76916.firebaseapp.com",
+  projectId: "memorial-book-76916",
+  storageBucket: "memorial-book-76916.appspot.com",
+  messagingSenderId: "558598960392",
+  appId: "1:558598960392:web:614f4a57034b42e01ba8bd",
+  measurementId: "G-LXP0DX5TWK"
+};
+
+export default initializeApp(firebaseInit);
+
+
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import './assets/css/main.scss'
 import { createPinia } from 'pinia'
+import './firebaseInit'
 
 const pinia = createPinia()
 const app = createApp(App)
