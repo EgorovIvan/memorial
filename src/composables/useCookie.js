514ebdeecb1df018aabb7c37433f5b46a228ff44
alexander <lokistapway@yandex.ru>
2023-11-26 17:13:04 +0300

    Добавлены слайдеры

--- src/composables/useCookie.js
+++ src/composables/useCookie.js
@@ -1,6 +1,7 @@
 export function useCookie() {
   function getCookie(name) {
     let matches = document.cookie.match(new RegExp(
+      // eslint-disable-next-line
       "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
     ));
     return matches ? decodeURIComponent(matches[1]) : undefined;
--- src/pages/NewsPage.vue
+++ src/pages/NewsPage.vue
@@ -1,9 +1,19 @@
 <template>
   <PageWrapper>
-    <div class="news-content">
+    <div class="news">
       <NewsSidebar
         :active-link="route.path"
       />
+      <div class="news-content">
+        <ProfileSlider
+            titleKey="newsPage.yourProfiles"
+            :profiles="data.related_humans"
+        />
+        <ProfileSlider
+            titleKey="newsPage.yourProfiles"
+            :profiles="data.pets"
+        />
+      </div>
     </div>
   </PageWrapper>
 </template>
@@ -12,17 +22,34 @@
 import PageWrapper from "@/components/common/PageWrapper.vue";
 import NewsSidebar from "@/components/NewsSidebar/NewsSidebar.vue";
 import {useRoute} from "vue-router";
+import ProfileSlider from "@/components/ProfileSlider/ProfileSlider.vue";
+import {onMounted, ref} from "vue";
+import api from "@/api/news/api";
 
 const route = useRoute()
+const data = ref([])
+
+onMounted(async () => {
+  data.value = (await api.getFeed()).data
+})
 </script>
 
 <style scoped lang="scss">
-.news-content {
+.news {
   width: 100%;
   min-height: calc(100vh - 116px);
-  display: flex;
+  display: grid;
+  grid-template-columns: auto 1fr;
+  align-items: start;
   background: $gray;
   margin-top: 96px;
   padding: 15px 0;
+
+  &-content {
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+    justify-self: center;
+  }
 }
 </style>
