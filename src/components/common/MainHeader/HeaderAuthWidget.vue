bb8bc3d1708bd09c58a0ee32a1e46f35e01d6988
alexander <lokistapway@yandex.ru>
2023-11-26 17:30:05 +0300

    Кнопка вынесена в отдельный компонент

---
+++ src/components/common/MainHeader/HeaderAuthWidget.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="auth-widget">
+    <button type="button" class="header-buttons__registration btn white-btn" @click="openRegPopup">
+      {{ $t('header.loginButton') }}
+    </button>
+  </div>
+</template>
+
+<script setup>
+  import {useRegistrationStore} from "@/store/registrationStore/useRegistrationStore";
+
+  const regStore = useRegistrationStore()
+
+  function openRegPopup() {
+    regStore.setVisibleRegistrationPopup(true)
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
